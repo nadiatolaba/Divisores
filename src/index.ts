d9b87c682daa2c2859d62ff57439bc5eea8458c5
Nadia Yanina Tolaba <103613141+nadiatolaba@users.noreply.github.com>
2022-05-15 19:54:02 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let numero: number = Number(prompt("Ingrese un número"));
+let numDivisores: number = cantDivisores(numero);
+console.log("el número", numero, "tiene", numDivisores, "divisores");
+
+function esMultiplo(numero1: number, numero2: number): boolean {
+  return numero1 % numero2 === 0;
+}
+
+function cantDivisores(numero: number): number {
+  let cantidad: number = 0;
+  let divisor: number;
+  for (divisor = 0; divisor < numero; divisor++) {
+    if (esMultiplo(numero, divisor)) {
+      cantidad++;
+    }
+  }
+
+  return cantidad;
+}
